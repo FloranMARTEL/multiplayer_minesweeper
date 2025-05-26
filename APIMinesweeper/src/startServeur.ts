f26ff756c696700296ff7bbec3742f2c8897cf92
Floran MARTEL <floran.martel@gmail.com>
2025-05-26 23:13:37 +0200

    web soket exemple

--- APIMinesweeper/src/startServeur.ts
+++ APIMinesweeper/src/startServeur.ts
@@ -1,9 +1,36 @@
 console.log("Server Start")
 
+const PORT = 5000
 
-import express, { Request, Response } from 'express';
-let app = express();
+// import express, { Request, Response } from 'express';
+// let app = express();
 
-app.use('/api', require('./root.js'));
+// app.use('/api', require('./root.js'));
 
-app.listen(5000);
+// app.listen(PORT);
+
+
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+const server = http.createServer();
+server.listen(PORT)
+
+
+const wsServer = new webSocketServer({
+  httpServer: server
+});
+
+
+const clients = [];
+
+wsServer.on('request', (request : any)=>{
+
+    console.log('org', request.origin + '.')
+
+    clients.push(request.accept(null,request.origin))
+})
+
+wsServer.on('message',( message : any) =>{
+    console.log(message.utf8Data);
+})
--- minesweeper/src/pages/Home/index.jsx
+++ minesweeper/src/pages/Home/index.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const client = new W3CWebSocket('ws://localhost:5000');
+
+
 export default class Home extends React.Component{
     
+    constructor(props){
+        super(props);
+        client.onopen = () =>{
+            console.log("connected");
+        }
+    }
 
+    onclick(){
+        console.log("bonjour");
+        client.send(JSON.stringify({
+            type : "message",
+            msg: "bonjour",
+        }))
+    }
 
     render(){
         return(
             <div>
-                Test
+                <button onClick={this.onclick}>Send</button>
             </div>
         )
     }
